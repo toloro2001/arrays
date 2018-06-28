aa07b08de736d7f925959c5eb1aff3ffbaf156ba
toloro2001 <rotonton000@warren.k12.in.us>
2018-06-28 10:44:09 -0400

    last

--- index.js
+++ index.js
@@ -40,7 +40,10 @@ for(var i=0; i<7; i++){
 }
 
 write("kata9",kata9)
-
-var kata10 = lotrCitiesArray.sort(length);
+var kata10=lotrCitiesArray.sort(function(a, b){
+    // ASC  -> a.length - b.length
+    // DESC -> b.length - a.length
+    return a.length - b.length;
+  });
 write("kata10", kata10)
 
